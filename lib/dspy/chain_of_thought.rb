74afdbc8feba82567e010eb0466da781bde8e564
Vicente Reig Rincón de Arellano <hey@vicente.services>
2025-03-16 19:44:48 +0100

    feat: json schemas with dry-schema
    not pretty, but works

--- lib/dspy/chain_of_thought.rb
+++ lib/dspy/chain_of_thought.rb
@@ -5,10 +5,10 @@ module DSPy
   # before providing a final answer.
   class ChainOfThought
     attr_reader :signature_class, :instance
-    
+
     def initialize(signature_class)
       @signature_class = signature_class
-      
+
       prompt_with_reasoning = <<~PROMPT
       Reasoning: Let's think step by step in order to #{@signature_class.description}
       
@@ -18,27 +18,11 @@ module DSPy
         output :reasoning, String, desc: prompt_with_reasoning
       end
     end
-    
+
     def call(**input_values)
       predictor = DSPy::Predict.new(signature_class)
-      
+
       predictor.call(**input_values)
     end
-    
-    private
-    
-    # Make this ChainOfThought instance behave like the signature instance
-    def to_s
-      @instance&.to_s || super
-    end
-    
-    def inspect
-      @instance&.inspect || super
-    end
-    
-    # Allow this class to be treated as its signature class for testing
-    def is_a?(klass)
-      @instance&.is_a?(klass) || super
-    end
   end
-end 
+end
