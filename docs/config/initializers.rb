9bcd4482d90876417eb7edb741b07629a4cb0d0a
Vicente Reig Rincón de Arellano <hey@vicente.services>
2025-07-11 22:09:44 +0200

    feat: automate version display in documentation site
    - Load DSPy version dynamically from lib/dspy/version.rb
    - Update landing page to display current version and release link
    - Remove manual version updates from documentation workflow
    
    This ensures the docs always show the current gem version without
    manual intervention when releasing new versions.

--- docs/config/initializers.rb
+++ docs/config/initializers.rb
@@ -1,8 +1,11 @@
 # Initializers allow you to configure Bridgetown and various plugins/gems used by
 # your site. This file is executed during the initialization process.
-#
-# Feel free to delete this file if you don't need any initializer functionality.
 
-# Bridgetown.configure do |config|
-#   # Add configuration options here
-# end
+Bridgetown.configure do |config|
+  # Load the DSPy version from the gem
+  version_file = File.expand_path("../../lib/dspy/version.rb", __dir__)
+  
+  require version_file
+  config.dspy_version = DSPy::VERSION
+  config.dspy_release_url = "https://github.com/vicentereig/dspy.rb/releases/tag/v#{DSPy::VERSION}"
+end
