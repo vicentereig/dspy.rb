d939ea5320a6f4f4e4efdfaa2f81f6cf88773d48
Vicente Reig Rincón de Arellano <hey@vicente.services>
2025-11-21 12:37:10 -0500

    Bump sibling gem versions to 1.0.1

--- lib/dspy/anthropic/version.rb
+++ lib/dspy/anthropic/version.rb
@@ -2,6 +2,6 @@
 
 module DSPy
   module Anthropic
-    VERSION = '1.0.0'
+    VERSION = '1.0.1'
   end
 end
--- lib/dspy/gepa/version.rb
+++ lib/dspy/gepa/version.rb
@@ -5,6 +5,6 @@ require_relative '../version'
 
 module DSPy
   module GEPA
-    VERSION = '1.0.0'
+    VERSION = '1.0.1'
   end
 end
